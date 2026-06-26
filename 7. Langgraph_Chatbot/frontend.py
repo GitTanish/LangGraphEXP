c113c07a860526bf4c03654f532987967840f38a
GitTanish <tanishsaroj6781@gmail.com>
2026-06-26 15:18:29 +0530

    dumb ui

--- 7. Langgraph_Chatbot/frontend.py
+++ 7. Langgraph_Chatbot/frontend.py
@@ -1,19 +1,28 @@
 import streamlit as st
 
-with st.chat_message('user'):
-    st.text('Hi')
 
-with st.chat_message('assistant'):
-    st.text("You've summoned the ancient one. I am the all-knowing, all-seeing.State your Question, and I shall provide you with the wisdom you seek. But beware, for my answers may be cryptic and profound. What is it that you wish to know?")
+# session state -> dict -> 
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = [] 
 
-with st.chat_message('user'):
-    st.text("Oh great and mighty one, I seek your guidance on the mysteries of the universe. Can you enlighten me about how to make mayonaise?")
 
-user_input=st.chat_input("Ask me anything...")
+# loop to load conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
+
+
+# {"role":"user", "content":"Hi"}
+# {"role":"assistant", "content":"Hello"}
+
+user_input =st.chat_input("Ask me anything...")
 
 if user_input:
+
+    st.session_state["message_history"].append({'role':'user','content':user_input})
     with st.chat_message('user'):
         st.text(user_input)
 
+    st.session_state["message_history"].append({'role':'assistant','content':user_input})
     with st.chat_message('assistant'):
-        st.text("Seriously bro?")
+        st.text(user_input)
